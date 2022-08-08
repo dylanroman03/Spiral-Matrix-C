21ed64f3bb2e8ae7b7d573b0f8c6f6b2fe47ace8
Dylan Roman <romand.buitrago@gmail.com>
2022-08-07 20:14:06 -0400

    Handling Corners

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void caracolMatrix(int n, int esq)
+void caracolMatrix(int n, int corner)
 {
   int matrix[n * n];
   int steps[4] = {1, n, -1, -n};
   int e = 0;
   int x = 1;
   int y = 0;
+
+  switch (corner)
+  {
+  case 2:
+    e = n - 1;
+    y = 1;
+    break;
+  case 3:
+    e = (n * n) - 1;
+    y = 2;
+    break;
+  case 4:
+    e = (n * n) - n;
+    y = 3;
+    break;
+  }
+
   int step = steps[y];
 
   for (int i = 0; i < n * n; i++)
@@ -15,7 +32,7 @@ void caracolMatrix(int n, int esq)
     matrix[i] = 0;
   }
 
-  matrix[esq] = 1;
+  // matrix[corner] = 1;
 
   for (int i = 1; i <= n * n; i++)
   {
@@ -58,5 +75,9 @@ void caracolMatrix(int n, int esq)
 int main()
 {
   system("cls");
-  caracolMatrix(5, 0);
+  int n;
+  printf("Enter size of the matrix: \t");
+  scanf("%d", &n);
+  system("cls");
+  caracolMatrix(n, 4);
 }
